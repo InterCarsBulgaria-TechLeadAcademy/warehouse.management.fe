ad5e7083486712a9c3d02e5d2162a403485b4f9b
Hristo Boyadzhiev <hristo_boyadzhiev@abv.bg>
2024-05-31 11:39:33 +0300

    put the isFinal inside the schem

--- src/schemas/vendorSchema.ts
+++ src/schemas/vendorSchema.ts
@@ -1,16 +1,15 @@
 import * as yup from 'yup'
 
-export const vendorSchema = yup
-  .object({
-    vendorName: yup
-      .string()
-      .required('newVendor.errors.name.required')
-      .max(50, 'newVendor.errors.name.max'),
-    vendorNumber: yup
-      .string()
-      .required('newVendor.errors.vendorNumber.required')
-      .max(25, 'newVendor.errors.vendorNumber.max')
-  })
-  .required()
+export const vendorSchema = yup.object({
+  vendorName: yup
+    .string()
+    .required('newVendor.errors.name.required')
+    .max(50, 'newVendor.errors.name.max'),
+  vendorNumber: yup
+    .string()
+    .required('newVendor.errors.vendorNumber.required')
+    .max(25, 'newVendor.errors.vendorNumber.max'),
+  isFinal: yup.boolean()
+})
 
 export type VendorFormData = yup.InferType<typeof vendorSchema>
