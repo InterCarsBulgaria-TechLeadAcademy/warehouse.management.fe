e3522357551b589baf1907914e99eceaa75a5998
Hristo Boyadzhiev <hristo_boyadzhiev@abv.bg>
2024-06-02 15:11:38 +0300

    update code

--- src/components/features/ActionsMenu.tsx
+++ src/components/features/ActionsMenu.tsx
@@ -1,38 +1,41 @@
+import React from 'react'
 import IconButton from '@mui/material/IconButton'
 import Menu from '@mui/material/Menu'
 import MenuItem from '@mui/material/MenuItem'
 import MoreHorizIcon from '@mui/icons-material/MoreHoriz'
-import React from 'react'
-import WarningActionDialog from '../shared/WarningActionDialog'
-import { useTranslation } from 'react-i18next'
+// import WarningActionDialog from '../shared/WarningActionDialog'
+// import { useTranslation } from 'react-i18next'
 
-export default function ActionsMenu() {
-  const { t: translate } = useTranslation()
+interface Action {
+  label: string
+  component: React.ReactNode
+}
+
+interface ActionsMenuProps {
+  actions: Action[]
+  handleClickOpen: () => void
+}
+
+export default function ActionsMenu({ actions, handleClickOpen }: ActionsMenuProps) {
+  // const { t: translate } = useTranslation()
   const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null)
   const open = Boolean(anchorEl)
-  const [selectedOption, setSelectedOption] = React.useState<string | null>(null)
+  const [selectedAction, setSelectedAction] = React.useState<Action | null>(null)
 
   const handleClick = (event: React.MouseEvent<HTMLElement>) => {
     setAnchorEl(event.currentTarget)
   }
+
   const handleClose = () => {
-    setSelectedOption(null)
+    setSelectedAction(null)
     setAnchorEl(null)
   }
 
-  const onDiscardClick = () => {
-    handleClose()
+  const actionHandler = (action: Action) => {
+    setSelectedAction(action)
+    // handleClose()
+    handleClickOpen()
   }
-
-  const onConfirmClick = () => {
-    handleClose()
-  }
-
-  const actionHandler = (option: string) => {
-    setSelectedOption(option)
-  }
-
-  const options = [translate('actionsMenu.options.edit'), translate('actionsMenu.options.delete')]
 
   return (
     <div>
@@ -53,25 +56,92 @@ export default function ActionsMenu() {
         anchorEl={anchorEl}
         open={open}
         onClose={handleClose}>
-        {options.map((option) => (
-          <MenuItem key={option} onClick={() => actionHandler(option)}>
-            {option}
+        {actions.map((action) => (
+          <MenuItem key={action.label} onClick={() => actionHandler(action)}>
+            {action.label}
           </MenuItem>
         ))}
       </Menu>
 
-      {selectedOption === 'Изтрий' && (
-        <WarningActionDialog
-          open={open}
-          title={translate('deleteAction.title')}
-          content={translate('deleteAction.message')}
-          discardText={translate('deleteAction.labels.discard')}
-          confirmText={translate('deleteAction.labels.confirm')}
-          onCloseDialog={handleClose}
-          onDiscardClick={onDiscardClick}
-          onConfirmClick={onConfirmClick}
-        />
-      )}
+      {selectedAction && selectedAction.component}
     </div>
   )
 }
+
+// import IconButton from '@mui/material/IconButton'
+// import Menu from '@mui/material/Menu'
+// import MenuItem from '@mui/material/MenuItem'
+// import MoreHorizIcon from '@mui/icons-material/MoreHoriz'
+// import React from 'react'
+// import WarningActionDialog from '../shared/WarningActionDialog'
+// import { useTranslation } from 'react-i18next'
+
+// export default function ActionsMenu() {
+//   const { t: translate } = useTranslation()
+//   const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null)
+//   const open = Boolean(anchorEl)
+//   const [selectedOption, setSelectedOption] = React.useState<string | null>(null)
+
+//   const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+//     setAnchorEl(event.currentTarget)
+//   }
+//   const handleClose = () => {
+//     setSelectedOption(null)
+//     setAnchorEl(null)
+//   }
+
+//   const onDiscardClick = () => {
+//     handleClose()
+//   }
+
+//   const onConfirmClick = () => {
+//     handleClose()
+//   }
+
+//   const actionHandler = (option: string) => {
+//     setSelectedOption(option)
+//   }
+
+//   const options = [translate('actionsMenu.options.edit'), translate('actionsMenu.options.delete')]
+
+//   return (
+//     <div>
+//       <IconButton
+//         aria-label="more"
+//         id="long-button"
+//         aria-controls={open ? 'long-menu' : undefined}
+//         aria-expanded={open ? 'true' : undefined}
+//         aria-haspopup="true"
+//         onClick={handleClick}>
+//         <MoreHorizIcon />
+//       </IconButton>
+//       <Menu
+//         id="long-menu"
+//         MenuListProps={{
+//           'aria-labelledby': 'long-button'
+//         }}
+//         anchorEl={anchorEl}
+//         open={open}
+//         onClose={handleClose}>
+//         {options.map((option) => (
+//           <MenuItem key={option} onClick={() => actionHandler(option)}>
+//             {option}
+//           </MenuItem>
+//         ))}
+//       </Menu>
+
+//       {selectedOption === 'Изтрий' && (
+//         <WarningActionDialog
+//           open={open}
+//           title={translate('deleteAction.title')}
+//           content={translate('deleteAction.message')}
+//           discardText={translate('deleteAction.labels.discard')}
+//           confirmText={translate('deleteAction.labels.confirm')}
+//           onCloseDialog={handleClose}
+//           onDiscardClick={onDiscardClick}
+//           onConfirmClick={onConfirmClick}
+//         />
+//       )}
+//     </div>
+//   )
+// }
--- src/components/features/VendorsTable.tsx
+++ src/components/features/VendorsTable.tsx
@@ -1,9 +1,29 @@
 import ActionsMenu from '@/components/features/ActionsMenu'
 import DataTable from '@/components/shared/DataTable'
+import WarningActionDialog from '../shared/WarningActionDialog'
 import { useTranslation } from 'react-i18next'
+import { useState } from 'react'
 
 export default function VendorsTable() {
   const { t: translate } = useTranslation()
+  const [openDialog, setOpenDialog] = useState(false)
+
+  const handleClickOpen = () => {
+    setOpenDialog(true)
+  }
+
+  const handleClose = () => {
+    setOpenDialog(false)
+  }
+
+  const onDiscardClick = () => {
+    handleClose()
+  }
+
+  const onConfirmClick = () => {
+    handleClose()
+  }
+
   return (
     <DataTable
       hasSearchInput={true}
@@ -17,9 +37,43 @@ export default function VendorsTable() {
         { key: 'actions', title: translate('vendors.table.actions'), minWidth: 50, align: 'right' }
       ]}
       rowData={[
-        { name: 'Bosch', vendorNumber: 1, markers: 'Масло', actions: <ActionsMenu /> },
-        { name: 'Valeo', vendorNumber: 2, markers: 'Чистачки', actions: <ActionsMenu /> }
+        {
+          name: 'Bosch',
+          vendorNumber: 1,
+          markers: 'Масло',
+          actions: (
+            <ActionsMenu
+              actions={[
+                {
+                  label: translate('actionsMenu.options.delete'),
+                  component: (
+                    <WarningActionDialog
+                      open={openDialog}
+                      title={translate('deleteAction.title')}
+                      content={translate('deleteAction.message')}
+                      discardText={translate('deleteAction.labels.discard')}
+                      confirmText={translate('deleteAction.labels.confirm')}
+                      onCloseDialog={handleClose}
+                      onDiscardClick={onDiscardClick}
+                      onConfirmClick={onConfirmClick}
+                    />
+                  )
+                }
+              ]}
+              handleClickOpen={handleClickOpen}
+            />
+          )
+        }
+        // ,
+        // {
+        //   name: 'Valeo',
+        //   vendorNumber: 2,
+        //   markers: 'Чистачки',
+        //   actions: <ActionsMenu actions={[{ label: translate('actionsMenu.options.delete') }]} />
+        // }
       ]}
     />
   )
 }
+
+// const options = [translate('actionsMenu.options.edit'), translate('actionsMenu.options.delete')]
