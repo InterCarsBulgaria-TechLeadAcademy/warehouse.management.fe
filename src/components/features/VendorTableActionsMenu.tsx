06449c4a74bde0b4a0f4e2c1e8ff288533e329ec
Hristo Boyadzhiev <hristo_boyadzhiev@abv.bg>
2024-06-02 16:03:23 +0300

    Rename ActionsMenu component

---
+++ src/components/features/VendorTableActionsMenu.tsx
@@ -0,0 +1,77 @@
+import IconButton from '@mui/material/IconButton'
+import Menu from '@mui/material/Menu'
+import MenuItem from '@mui/material/MenuItem'
+import MoreHorizIcon from '@mui/icons-material/MoreHoriz'
+import React from 'react'
+import WarningActionDialog from '../shared/WarningActionDialog'
+import { useTranslation } from 'react-i18next'
+
+export default function VendorTableActionsMenu() {
+  const { t: translate } = useTranslation()
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null)
+  const open = Boolean(anchorEl)
+  const [selectedOption, setSelectedOption] = React.useState<string | null>(null)
+
+  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget)
+  }
+  const handleClose = () => {
+    setSelectedOption(null)
+    setAnchorEl(null)
+  }
+
+  const onDiscardClick = () => {
+    handleClose()
+  }
+
+  const onConfirmClick = () => {
+    handleClose()
+  }
+
+  const actionHandler = (option: string) => {
+    setSelectedOption(option)
+  }
+
+  const options = [translate('actionsMenu.options.edit'), translate('actionsMenu.options.delete')]
+
+  return (
+    <div>
+      <IconButton
+        aria-label="more"
+        id="long-button"
+        aria-controls={open ? 'long-menu' : undefined}
+        aria-expanded={open ? 'true' : undefined}
+        aria-haspopup="true"
+        onClick={handleClick}>
+        <MoreHorizIcon />
+      </IconButton>
+      <Menu
+        id="long-menu"
+        MenuListProps={{
+          'aria-labelledby': 'long-button'
+        }}
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}>
+        {options.map((option) => (
+          <MenuItem key={option} onClick={() => actionHandler(option)}>
+            {option}
+          </MenuItem>
+        ))}
+      </Menu>
+
+      {selectedOption === 'Изтрий' && (
+        <WarningActionDialog
+          open={open}
+          title={translate('deleteAction.title')}
+          content={translate('deleteAction.message')}
+          discardText={translate('deleteAction.labels.discard')}
+          confirmText={translate('deleteAction.labels.confirm')}
+          onCloseDialog={handleClose}
+          onDiscardClick={onDiscardClick}
+          onConfirmClick={onConfirmClick}
+        />
+      )}
+    </div>
+  )
+}
--- src/components/features/VendorsTable.tsx
+++ src/components/features/VendorsTable.tsx
@@ -1,28 +1,9 @@
-import ActionsMenu from '@/components/features/ActionsMenu'
+import VendorTableActionsMenu from '@/components/features/VendorTableActionsMenu'
 import DataTable from '@/components/shared/DataTable'
-import WarningActionDialog from '../shared/WarningActionDialog'
 import { useTranslation } from 'react-i18next'
-import { useState } from 'react'
 
 export default function VendorsTable() {
   const { t: translate } = useTranslation()
-  const [openDialog, setOpenDialog] = useState(false)
-
-  const handleClickOpen = () => {
-    setOpenDialog(true)
-  }
-
-  const handleClose = () => {
-    setOpenDialog(false)
-  }
-
-  const onDiscardClick = () => {
-    handleClose()
-  }
-
-  const onConfirmClick = () => {
-    handleClose()
-  }
 
   return (
     <DataTable
@@ -41,39 +22,15 @@ export default function VendorsTable() {
           name: 'Bosch',
           vendorNumber: 1,
           markers: 'Масло',
-          actions: (
-            <ActionsMenu
-              actions={[
-                {
-                  label: translate('actionsMenu.options.delete'),
-                  component: (
-                    <WarningActionDialog
-                      open={openDialog}
-                      title={translate('deleteAction.title')}
-                      content={translate('deleteAction.message')}
-                      discardText={translate('deleteAction.labels.discard')}
-                      confirmText={translate('deleteAction.labels.confirm')}
-                      onCloseDialog={handleClose}
-                      onDiscardClick={onDiscardClick}
-                      onConfirmClick={onConfirmClick}
-                    />
-                  )
-                }
-              ]}
-              handleClickOpen={handleClickOpen}
-            />
-          )
+          actions: <VendorTableActionsMenu />
+        },
+        {
+          name: 'Valeo',
+          vendorNumber: 2,
+          markers: 'Чистачки',
+          actions: <VendorTableActionsMenu />
         }
-        // ,
-        // {
-        //   name: 'Valeo',
-        //   vendorNumber: 2,
-        //   markers: 'Чистачки',
-        //   actions: <ActionsMenu actions={[{ label: translate('actionsMenu.options.delete') }]} />
-        // }
       ]}
     />
   )
 }
-
-// const options = [translate('actionsMenu.options.edit'), translate('actionsMenu.options.delete')]
