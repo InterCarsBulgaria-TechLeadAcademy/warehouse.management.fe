21d578ec88703bec9dcbe0cdc7e793e2440ce111
Hristo Boyadzhiev <hristo_boyadzhiev@abv.bg>
2024-06-03 09:41:14 +0300

    Update routes

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -5,13 +5,16 @@ import {
   DEFAULTLAYOUT_PATH,
   PROJECTS_PATH_EXAMPLE,
   VENDORS_PATH
+  // ZONES_PATH
 } from '@/router/routerPaths.ts'
+// import Zones from '@/pages/Zones'
 
 const Home = lazy(() => import('@/pages/Home.tsx'))
 const Projects = lazy(() => import('@/pages/Projects.tsx'))
 const Login = lazy(() => import('@/pages/Login.tsx'))
 const DefaultLayout = lazy(() => import('@/layouts/DefaultLayout.tsx'))
 const Vendors = lazy(() => import('@/pages/Vendors.tsx'))
+// const Zones = lazy(() => import('@/pages/Zones.tsx'))
 
 export default function Router() {
   return useRoutes([
@@ -43,5 +46,13 @@ export default function Router() {
         </DefaultLayout>
       )
     }
+    // {
+    //   path: ZONES_PATH,
+    //   element: (
+    //     <DefaultLayout>
+    //       <Zones />
+    //     </DefaultLayout>
+    //   )
+    // }
   ])
 }
