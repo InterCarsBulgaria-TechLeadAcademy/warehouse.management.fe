e1a878727217778a8004b1add5dc55ccc0892516
Hristo Yonkov <hristopturs@gmail.com>
2024-06-07 16:44:59 +0300

    add zones page to router

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -5,7 +5,8 @@ import {
   DEFAULTLAYOUT_PATH,
   PROJECTS_PATH_EXAMPLE,
   VENDORS_PATH,
-  MARKERS_PATH
+  MARKERS_PATH,
+  ZONES_PATH
 } from '@/router/routerPaths.ts'
 import Markers from '@/pages/Markers'
 
@@ -14,6 +15,7 @@ const Projects = lazy(() => import('@/pages/Projects.tsx'))
 const Login = lazy(() => import('@/pages/Login.tsx'))
 const DefaultLayout = lazy(() => import('@/layouts/DefaultLayout.tsx'))
 const Vendors = lazy(() => import('@/pages/Vendors.tsx'))
+const Zones = lazy(() => import('@/pages/Zones.tsx'))
 
 export default function Router() {
   return useRoutes([
@@ -50,6 +52,14 @@ export default function Router() {
       element: (
         <DefaultLayout>
           <Markers />
+        </DefaultLayout>
+      )
+    },
+    {
+      path: ZONES_PATH,
+      element: (
+        <DefaultLayout>
+          <Zones />
         </DefaultLayout>
       )
     }
