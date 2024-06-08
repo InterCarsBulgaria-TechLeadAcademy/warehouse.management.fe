9105bb289cfecf2f17cbf5462f1348bc8a4b4379
Hristo Yonkov <hristopturs@gmail.com>
2024-06-08 15:33:17 +0300

    add search and sort components

--- src/components/features/VendorsTable.tsx
+++ src/components/features/VendorsTable.tsx
@@ -1,37 +1,65 @@
 import VendorTableActionsMenu from '@/components/features/VendorTableActionsMenu'
 import DataTable from '@/components/shared/DataTable'
+import React from 'react'
 import { useTranslation } from 'react-i18next'
+import { Column } from '@/interfaces/dataTable'
+import SearchInput from './SearchInput'
+import { Autocomplete, TextField } from '@mui/material'
+
 
 export default function VendorsTable() {
   const { t: translate } = useTranslation()
+  const [searchTerm, setSearchTerm] = React.useState('')
+
+  const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchTerm(event.target.value)
+  }
+
+  let sortOptions = ['regular', 'admin']
+  let options = sortOptions.map((option) => ({ label: option }))
+
+  const columnsData: Column[] = [
+    { key: 'name', title: translate('vendors.table.name') },
+    { key: 'vendorNumber', title: translate('vendors.table.vendorNumber') },
+    { key: 'markers', title: translate('vendors.table.markers') },
+    { key: 'actions', title: translate('vendors.table.actions'), minWidth: 50, align: 'right' }
+  ]
+
+  const rowData = [
+    {
+      name: 'Bosch',
+      vendorNumber: 1,
+      markers: 'Масло',
+      actions: <VendorTableActionsMenu />
+    },
+    {
+      name: 'Valeo',
+      vendorNumber: 2,
+      markers: 'Чистачки',
+      actions: <VendorTableActionsMenu />
+    }
+  ]
 
   return (
     <DataTable
-      hasSearchInput={true}
-      isSortTextField={true}
-      isToggle={false}
-      sortLabel={translate('vendors.labels.role')}
-      sortOptionsData={['Proba1', 'Proba2']}
-      columnsData={[
-        { key: 'name', title: translate('vendors.table.name') },
-        { key: 'vendorNumber', title: translate('vendors.table.vendorNumber') },
-        { key: 'markers', title: translate('vendors.table.markers') },
-        { key: 'actions', title: translate('vendors.table.actions'), minWidth: 50, align: 'right' }
-      ]}
-      rowData={[
-        {
-          name: 'Bosch',
-          vendorNumber: 1,
-          markers: 'Масло',
-          actions: <VendorTableActionsMenu />
-        },
-        {
-          name: 'Valeo',
-          vendorNumber: 2,
-          markers: 'Чистачки',
-          actions: <VendorTableActionsMenu />
-        }
-      ]}
-    />
+      columnsData={columnsData}
+      rowData={rowData}
+    >
+      <SearchInput
+        value={searchTerm}
+        onChange={handleSearchChange}
+        placeholder={translate('vendors.labels.search')}
+      />
+
+      <Autocomplete
+        disablePortal
+        id="combo-box-demo"
+        options={options}
+        size="small"
+        sx={{ width: '235px' }}
+        renderInput={(params) => <TextField {...params} label={translate('vendors.labels.role')} />}
+      />
+
+    </DataTable>
   )
 }
