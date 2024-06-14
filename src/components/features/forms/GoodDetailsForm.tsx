506e84d1bd0852dbd4a7febfea48f546d0f02f66
Hristo Boyadzhiev <hristo_boyadzhiev@abv.bg>
2024-06-14 11:55:36 +0300

    good update code

--- src/components/features/forms/GoodDetailsForm.tsx
+++ src/components/features/forms/GoodDetailsForm.tsx
@@ -13,25 +13,29 @@ interface GoodDetailsFormProps {
   errors: FieldErrors<NewDeliveryStep3FormData>
   goodType: string[]
   deleteHandler: () => void
+  index: number
+  formsCount: number
 }
 
-// При "Назад" oт 4 на 3 стъпка Autocomplete е празно, а количеството се запазва.
-// Ако дам направо "напред" празното Autocomplete не се валидира отново.
 export default function GoodDetailsForm({
   control,
   errors,
   goodType,
-  deleteHandler
+  deleteHandler,
+  index,
+  formsCount
 }: GoodDetailsFormProps) {
   const { t: translate } = useTranslation()
 
   const [value, setValue] = React.useState<string | null>(null)
   const [inputValue, setInputValue] = React.useState('')
 
+  console.log(formsCount)
+
   return (
-    <Box component="form" sx={{ display: 'flex', gap: '1em', alignItems: 'center' }}>
+    <Box sx={{ display: 'flex', gap: '1em', alignItems: 'center' }}>
       <Controller
-        name="goodType"
+        name={`goods.${index}.goodType`} // Използване на индекса за уникално име на полето
         control={control}
         render={({ field }) => (
           <Autocomplete
@@ -45,7 +49,7 @@ export default function GoodDetailsForm({
             onInputChange={(event, newInputValue) => {
               setInputValue(newInputValue)
             }}
-            id="controllable-states-demo"
+            id={`controllable-states-demo-${index}`} // Уникален ID за Autocomplete
             options={goodType}
             sx={{ flex: 1 }}
             renderInput={(params) => (
@@ -53,33 +57,123 @@ export default function GoodDetailsForm({
                 {...params}
                 required
                 label={translate('newDelivery.labels.step3.goodType')}
-                error={!!errors.goodType}
-                helperText={errors.goodType?.message ? translate(errors.goodType.message) : ''}
+                error={!!errors?.goods?.[index]?.goodType}
+                helperText={
+                  errors?.goods?.[index]?.goodType?.message
+                    ? translate(errors.goods[index].goodType.message) //не мога да го оправя
+                    : ''
+                }
               />
             )}
           />
         )}
       />
-
       <Controller
-        name="goodQuantity"
+        name={`goods.${index}.goodQuantity`} // Използване на индекса за уникално име на полето
         control={control}
         render={({ field }) => (
           <TextField
             {...field}
             label={translate('newDelivery.labels.step3.goodQuantity')}
-            id="goodQuantity"
-            name="goodQuantity"
+            id={`goodQuantity-${index}`} // Уникален ID за TextField
+            name={`goodQuantity-${index}`} // Уникално име за полето
             sx={{ flex: 1 }}
             required
-            error={!!errors.goodQuantity}
-            helperText={errors.goodQuantity?.message ? translate(errors.goodQuantity.message) : ''}
+            error={!!errors?.goods?.[index]?.goodQuantity}
+            helperText={
+              errors?.goods?.[index]?.goodQuantity?.message
+                ? translate(errors.goods[index].goodQuantity.message)
+                : ''
+            }
           />
         )}
       />
-      {!errors.goodType && !errors.goodQuantity ? (
-        <DeleteIcon sx={{ cursor: 'pointer' }} onClick={deleteHandler} />
-      ) : null}
+      {formsCount > 1 ? <DeleteIcon sx={{ cursor: 'pointer' }} onClick={deleteHandler} /> : null}
     </Box>
   )
 }
+
+// import { Box } from '@mui/material'
+// import { Control, Controller, FieldErrors } from 'react-hook-form'
+// import DeleteIcon from '@mui/icons-material/Delete'
+// import { NewDeliveryStep3FormData } from '@/schemas/newDeliveryStep3'
+// import { useTranslation } from 'react-i18next'
+
+// import TextField from '@mui/material/TextField'
+// import Autocomplete from '@mui/material/Autocomplete'
+// import React from 'react'
+
+// interface GoodDetailsFormProps {
+//   control: Control<NewDeliveryStep3FormData, any>
+//   errors: FieldErrors<NewDeliveryStep3FormData>
+//   goodType: string[]
+//   deleteHandler: () => void
+// }
+
+// // При "Назад" oт 4 на 3 стъпка Autocomplete е празно, а количеството се запазва.
+// // Ако дам направо "напред" празното Autocomplete не се валидира отново.
+// export default function GoodDetailsForm({
+//   control,
+//   errors,
+//   goodType,
+//   deleteHandler
+// }: GoodDetailsFormProps) {
+//   const { t: translate } = useTranslation()
+
+//   const [value, setValue] = React.useState<string | null>(null)
+//   const [inputValue, setInputValue] = React.useState('')
+
+//   return (
+//     <Box sx={{ display: 'flex', gap: '1em', alignItems: 'center' }}>
+//       <Controller
+//         name="goodType"
+//         control={control}
+//         render={({ field }) => (
+//           <Autocomplete
+//             {...field}
+//             value={value}
+//             onChange={(event: any, newValue: string | null) => {
+//               setValue(newValue)
+//               field.onChange(newValue)
+//             }}
+//             inputValue={inputValue}
+//             onInputChange={(event, newInputValue) => {
+//               setInputValue(newInputValue)
+//             }}
+//             id="controllable-states-demo"
+//             options={goodType}
+//             sx={{ flex: 1 }}
+//             renderInput={(params) => (
+//               <TextField
+//                 {...params}
+//                 required
+//                 label={translate('newDelivery.labels.step3.goodType')}
+//                 error={!!errors.goodType}
+//                 helperText={errors.goodType?.message ? translate(errors.goodType.message) : ''}
+//               />
+//             )}
+//           />
+//         )}
+//       />
+//       <Controller
+//         name="goodQuantity"
+//         control={control}
+//         render={({ field }) => (
+//           <TextField
+//             {...field}
+//             label={translate('newDelivery.labels.step3.goodQuantity')}
+//             id="goodQuantity"
+//             name="goodQuantity"
+//             sx={{ flex: 1 }}
+//             required
+//             error={!!errors.goodQuantity}
+//             helperText={errors.goodQuantity?.message ? translate(errors.goodQuantity.message) : ''}
+//           />
+//         )}
+//       />
+//       {!errors.goodType && !errors.goodQuantity ? (
+//         <DeleteIcon sx={{ cursor: 'pointer' }} onClick={deleteHandler} />
+//       ) : null}
+//     </Box>
+//   )
+// }
--- src/components/features/forms/NewDeliveryStep3Form.tsx
+++ src/components/features/forms/NewDeliveryStep3Form.tsx
@@ -3,35 +3,47 @@ import { useTranslation } from 'react-i18next'
 import GoodDetailsForm from './GoodDetailsForm'
 import { UseFormReturn } from 'react-hook-form'
 import { NewDeliveryStep3FormData } from '@/schemas/newDeliveryStep3'
+import { useState } from 'react'
 
 export default function NewDeliveryStep3Form({
   control,
   formState: { errors }
 }: UseFormReturn<NewDeliveryStep3FormData>) {
   const { t: translate } = useTranslation()
+
   const goodType = [
     translate('newDelivery.goodType.pallets'),
     translate('newDelivery.goodType.packages'),
     translate('newDelivery.goodType.pieces')
   ]
 
-  function deleteHandler() {
-    console.log('deleted')
+  //Списък с индексите на формите
+  const [goodDetailsForms, setGoodDetailsForms] = useState<number[]>([0])
+
+  function deleteHandler(index: number) {
+    const updatedForms = goodDetailsForms.filter((_, id) => id !== index)
+    setGoodDetailsForms(updatedForms)
   }
 
   function addGoodHandler() {
-    console.log('added')
+    const newIndex = goodDetailsForms.length // Нов индекс за новата форма
+    setGoodDetailsForms([...goodDetailsForms, newIndex])
   }
 
   return (
     <>
-      <GoodDetailsForm
-        control={control}
-        errors={errors}
-        goodType={goodType}
-        deleteHandler={deleteHandler}
-      />
-
+      {goodDetailsForms.map((index) => (
+        <GoodDetailsForm
+          key={index}
+          control={control}
+          errors={errors}
+          goodType={goodType}
+          deleteHandler={() => deleteHandler(index)}
+          index={index} // Предаване на индекса като проп
+          formsCount={goodDetailsForms.length} //Предаване на броя на формите, за да контролиране
+          // показването на delete button
+        />
+      ))}
       <Button variant="contained" sx={{ alignSelf: 'flex-start' }} onClick={addGoodHandler}>
         {translate('newDelivery.labels.step3.addGood')}
       </Button>
@@ -39,96 +51,137 @@ export default function NewDeliveryStep3Form({
   )
 }
 
-// import { NewDeliveryStep3FormData } from '@/schemas/newDeliveryStep3'
-// import {
-//   Box,
-//   Checkbox,
-//   FormControl,
-//   FormHelperText,
-//   InputLabel,
-//   ListItemText,
-//   MenuItem,
-//   OutlinedInput,
-//   Select,
-//   TextField
-// } from '@mui/material'
-// import { Controller, UseFormReturn } from 'react-hook-form'
+// import { Button } from '@mui/material'
 // import { useTranslation } from 'react-i18next'
-// import DeleteIcon from '@mui/icons-material/Delete'
+// import GoodDetailsForm from './GoodDetailsForm'
+// import { UseFormReturn } from 'react-hook-form'
+// import { NewDeliveryStep3FormData } from '@/schemas/newDeliveryStep3'
+// import { useState } from 'react'
 
 // export default function NewDeliveryStep3Form({
 //   control,
 //   formState: { errors }
 // }: UseFormReturn<NewDeliveryStep3FormData>) {
 //   const { t: translate } = useTranslation()
-
-//   //изнеси го от тук
 //   const goodType = [
 //     translate('newDelivery.goodType.pallets'),
 //     translate('newDelivery.goodType.packages'),
 //     translate('newDelivery.goodType.pieces')
 //   ]
 
-//   function deleteHandler() {
-//     console.log('clicked')
-//   }
+//   const [goodDetailsFormCount, setGoodDetailsFormCount] = useState(1)
+
+//   function deleteHandler() {}
+
+//   function addGoodHandler() {}
 
 //   return (
-//     <Box component="form" sx={{ display: 'flex', gap: '1em', alignItems: 'center' }}>
-//       <Controller
-//         name="goodType"
+//     <>
+//       <GoodDetailsForm
 //         control={control}
-//         render={({ field }) => (
-//           <FormControl sx={{ flex: 1 }}>
-//             <InputLabel id="demo-multiple-checkbox-label" required>
-//               {translate('newDelivery.labels.step3.goodType')}
-//             </InputLabel>
-//             <Select
-//               {...field}
-//               labelId="demo-multiple-checkbox-label"
-//               id="demo-multiple-checkbox"
-//               required
-//               multiple
-//               value={field.value || []}
-//               onChange={(e) => field.onChange(e.target.value)}
-//               input={<OutlinedInput />}
-//               renderValue={(selected) => (selected as string[]).join(', ')}>
-//               {goodType.map((currentGoodType) => (
-//                 <MenuItem key={currentGoodType} value={currentGoodType}>
-//                   <Checkbox checked={field.value?.includes(currentGoodType)} />{' '}
-//                   <ListItemText primary={currentGoodType} />
-//                 </MenuItem>
-//               ))}
-//             </Select>
-//             {errors.goodType && (
-//               <FormHelperText>
-//                 {errors.goodType?.message ? translate(errors.goodType.message) : ''}
-//               </FormHelperText>
-//             )}
-//           </FormControl>
-//         )}
+//         errors={errors}
+//         goodType={goodType}
+//         deleteHandler={deleteHandler}
 //       />
-
-//       <Controller
-//         name="goodQuantity"
-//         control={control}
-//         render={({ field }) => (
-//           <TextField
-//             {...field}
-//             label={translate('newDelivery.labels.step3.goodQuantity')}
-//             id="goodQuantity"
-//             name="goodQuantity"
-//             sx={{
-//               flex: 1
-//             }}
-//             required
-//             error={!!errors.goodQuantity}
-//             helperText={errors.goodQuantity?.message ? translate(errors.goodQuantity.message) : ''}
-//           />
-//         )}
-//       />
-
-//       <DeleteIcon sx={{ cursor: 'pointer' }} onClick={deleteHandler} />
-//     </Box>
+//       <Button variant="contained" sx={{ alignSelf: 'flex-start' }} onClick={addGoodHandler}>
+//         {translate('newDelivery.labels.step3.addGood')}
+//       </Button>
+//     </>
 //   )
 // }
+
+{
+  /* import { NewDeliveryStep3FormData } from '@/schemas/newDeliveryStep3'
+import {
+  Box,
+  Checkbox,
+  FormControl,
+  FormHelperText,
+  InputLabel,
+  ListItemText,
+  MenuItem,
+  OutlinedInput,
+  Select,
+  TextField
+} from '@mui/material'
+import { Controller, UseFormReturn } from 'react-hook-form'
+import { useTranslation } from 'react-i18next'
+import DeleteIcon from '@mui/icons-material/Delete'
+
+export default function NewDeliveryStep3Form({
+  control,
+  formState: { errors }
+}: UseFormReturn<NewDeliveryStep3FormData>) {
+  const { t: translate } = useTranslation()
+
+  //изнеси го от тук
+  const goodType = [
+    translate('newDelivery.goodType.pallets'),
+    translate('newDelivery.goodType.packages'),
+    translate('newDelivery.goodType.pieces')
+  ]
+
+  function deleteHandler() {
+    console.log('clicked')
+  }
+
+  return (
+    <Box component="form" sx={{ display: 'flex', gap: '1em', alignItems: 'center' }}>
+      <Controller
+        name="goodType"
+        control={control}
+        render={({ field }) => (
+          <FormControl sx={{ flex: 1 }}>
+            <InputLabel id="demo-multiple-checkbox-label" required>
+              {translate('newDelivery.labels.step3.goodType')}
+            </InputLabel>
+            <Select
+              {...field}
+              labelId="demo-multiple-checkbox-label"
+              id="demo-multiple-checkbox"
+              required
+              multiple
+              value={field.value || []}
+              onChange={(e) => field.onChange(e.target.value)}
+              input={<OutlinedInput />}
+              renderValue={(selected) => (selected as string[]).join(', ')}>
+              {goodType.map((currentGoodType) => (
+                <MenuItem key={currentGoodType} value={currentGoodType}>
+                  <Checkbox checked={field.value?.includes(currentGoodType)} />{' '}
+                  <ListItemText primary={currentGoodType} />
+                </MenuItem>
+              ))}
+            </Select>
+            {errors.goodType && (
+              <FormHelperText>
+                {errors.goodType?.message ? translate(errors.goodType.message) : ''}
+              </FormHelperText>
+            )}
+          </FormControl>
+        )}
+      />
+
+      <Controller
+        name="goodQuantity"
+        control={control}
+        render={({ field }) => (
+          <TextField
+            {...field}
+            label={translate('newDelivery.labels.step3.goodQuantity')}
+            id="goodQuantity"
+            name="goodQuantity"
+            sx={{
+              flex: 1
+            }}
+            required
+            error={!!errors.goodQuantity}
+            helperText={errors.goodQuantity?.message ? translate(errors.goodQuantity.message) : ''}
+          />
+        )}
+      />
+
+      <DeleteIcon sx={{ cursor: 'pointer' }} onClick={deleteHandler} />
+    </Box>
+  )
+} */
+}
