7b1073313ba64be2ff0a9b9930a1ceb5d44eab64
Hristo Boyadzhiev <hristo_boyadzhiev@abv.bg>
2024-06-22 15:52:28 +0300

    update

--- src/components/features/forms/NewDeliveryStep3Form.tsx
+++ src/components/features/forms/NewDeliveryStep3Form.tsx
@@ -1,6 +1,3 @@
-// Да го направя както step4 с уникални goodDetailsForms.
-// Да се редактира и GoodDetailsForm
-
 import { Button } from '@mui/material'
 import { useTranslation } from 'react-i18next'
 import GoodDetailsForm from './GoodDetailsForm'
@@ -18,7 +15,7 @@ export default function NewDeliveryStep3Form({
 
   const { remove, append } = useFieldArray({
     control,
-    name: 'goods' //the path to the goods array in formsData object, which we need to manage dynamically.
+    name: 'goods'
   })
 
   const initialGoodType = [
@@ -27,7 +24,6 @@ export default function NewDeliveryStep3Form({
     translate('newDelivery.goodType.pieces')
   ]
 
-  // Initialize goodDetailsForms and selectedGoodTypes based on formsData.goods
   const goodDetailsFormsInitialValue = formsData.goods
     ? formsData.goods.map((_: any, index: number) => index)
     : [0]
@@ -41,14 +37,13 @@ export default function NewDeliveryStep3Form({
   )
 
   function onDeleteHandler(index: number) {
-    // Не работи както трябва
-    remove(index) // This removes the item from goods array in formsData object
+    remove(index)
     setGoodDetailsForms((prev) => prev.filter((_, id) => id !== index))
     setSelectedGoodTypes((prev) => prev.filter((_, id) => id !== index))
   }
 
   function addGoodHandler() {
-    append({ goodTypeStep3: '', goodQuantityStep3: 0 }) // add this object in goods array in formsData object
+    append({ goodTypeStep3: '', goodQuantityStep3: 0 })
     setGoodDetailsForms((prev) => [...prev, prev.length])
     setSelectedGoodTypes((prev) => [...prev, ''])
   }
@@ -56,7 +51,7 @@ export default function NewDeliveryStep3Form({
   function handleGoodTypeChange(index: number, value: string | null) {
     setSelectedGoodTypes((prev) => {
       const updated = [...prev]
-      updated[index] = value || '' // Convert null to empty string
+      updated[index] = value || ''
       return updated
     })
   }
@@ -72,7 +67,6 @@ export default function NewDeliveryStep3Form({
 
   useEffect(() => {
     if (formsData.goods) {
-      // Update form values when formsData changes
       setGoodDetailsForms(formsData.goods.map((_: any, index: number) => index))
       setSelectedGoodTypes(formsData.goods.map((good: any) => good.goodTypeStep3 || ''))
     }
@@ -89,7 +83,7 @@ export default function NewDeliveryStep3Form({
           onDeleteHandler={() => onDeleteHandler(index)}
           index={index}
           formsCount={goodDetailsForms.length}
-          onGoodTypeChange={(value) => handleGoodTypeChange(index, value)} // Pass a function to change the item type
+          onGoodTypeChange={(value) => handleGoodTypeChange(index, value)}
         />
       ))}
       <Button
@@ -102,3 +96,105 @@ export default function NewDeliveryStep3Form({
     </>
   )
 }
+
+// import { Button } from '@mui/material'
+// import { useTranslation } from 'react-i18next'
+// import GoodDetailsForm from './GoodDetailsForm'
+// import { UseFormReturn, useFieldArray } from 'react-hook-form'
+// import { useState, useEffect } from 'react'
+// import { NewDeliveryStep3FormData } from '@/schemas/newDeliverySchemas'
+// import { useNewDeliveryContext } from '@/hooks/useNewDeliveryContext'
+
+// export default function NewDeliveryStep3Form({
+//   control,
+//   formState: { errors }
+// }: UseFormReturn<NewDeliveryStep3FormData>) {
+//   const { t: translate } = useTranslation()
+//   const { formsData } = useNewDeliveryContext()
+
+//   const { remove, append } = useFieldArray({
+//     control,
+//     name: 'goods' //the path to the goods array in formsData object, which we need to manage dynamically.
+//   })
+
+//   const initialGoodType = [
+//     translate('newDelivery.goodType.pallets'),
+//     translate('newDelivery.goodType.packages'),
+//     translate('newDelivery.goodType.pieces')
+//   ]
+
+//   // Initialize goodDetailsForms and selectedGoodTypes based on formsData.goods
+//   const goodDetailsFormsInitialValue = formsData.goods
+//     ? formsData.goods.map((_: any, index: number) => index)
+//     : [0]
+//   const selectedGoodTypesInitialValue = formsData.goods
+//     ? formsData.goods.map((good: any) => good.goodTypeStep3 || '')
+//     : ['']
+
+//   const [goodDetailsForms, setGoodDetailsForms] = useState<number[]>(goodDetailsFormsInitialValue)
+//   const [selectedGoodTypes, setSelectedGoodTypes] = useState<string[]>(
+//     selectedGoodTypesInitialValue
+//   )
+
+//   function onDeleteHandler(index: number) {
+//     // Не работи както трябва
+//     remove(index) // This removes the item from goods array in formsData object
+//     setGoodDetailsForms((prev) => prev.filter((_, id) => id !== index))
+//     setSelectedGoodTypes((prev) => prev.filter((_, id) => id !== index))
+//   }
+
+//   function addGoodHandler() {
+//     append({ goodTypeStep3: '', goodQuantityStep3: 0 }) // add this object in goods array in formsData object
+//     setGoodDetailsForms((prev) => [...prev, prev.length])
+//     setSelectedGoodTypes((prev) => [...prev, ''])
+//   }
+
+//   function handleGoodTypeChange(index: number, value: string | null) {
+//     setSelectedGoodTypes((prev) => {
+//       const updated = [...prev]
+//       updated[index] = value || '' // Convert null to empty string
+//       return updated
+//     })
+//   }
+
+//   function availableGoodTypes(index: number) {
+//     return initialGoodType.filter(
+//       (type) => !selectedGoodTypes.includes(type) || selectedGoodTypes[index] === type
+//     )
+//   }
+
+//   const allOptionsSelected =
+//     selectedGoodTypes.filter((type) => type !== '').length >= initialGoodType.length
+
+//   useEffect(() => {
+//     if (formsData.goods) {
+//       // Update form values when formsData changes
+//       setGoodDetailsForms(formsData.goods.map((_: any, index: number) => index))
+//       setSelectedGoodTypes(formsData.goods.map((good: any) => good.goodTypeStep3 || ''))
+//     }
+//   }, [formsData])
+
+//   return (
+//     <>
+//       {goodDetailsForms.map((index) => (
+//         <GoodDetailsForm
+//           key={index}
+//           control={control}
+//           errors={errors}
+//           goodType={availableGoodTypes(index)}
+//           onDeleteHandler={() => onDeleteHandler(index)}
+//           index={index}
+//           formsCount={goodDetailsForms.length}
+//           onGoodTypeChange={(value) => handleGoodTypeChange(index, value)} // Pass a function to change the item type
+//         />
+//       ))}
+//       <Button
+//         variant="contained"
+//         sx={{ alignSelf: 'flex-start' }}
+//         onClick={addGoodHandler}
+//         disabled={allOptionsSelected}>
+//         {translate('newDelivery.labels.step3.addGood')}
+//       </Button>
+//     </>
+//   )
+// }
