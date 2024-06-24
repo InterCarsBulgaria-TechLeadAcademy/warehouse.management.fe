0d2e1558992f8353a1e5d5629695ad3c98f1ac55
vexleet <abda70226@gmail.com>
2024-06-24 18:28:48 +0300

    fix step 3 form not showing data correctly

--- src/components/features/forms/NewDeliveryStep3Form.tsx
+++ src/components/features/forms/NewDeliveryStep3Form.tsx
@@ -5,6 +5,9 @@ import { UseFormReturn, useFieldArray } from 'react-hook-form'
 import { useState, useEffect } from 'react'
 import { NewDeliveryStep3FormData } from '@/schemas/newDeliverySchemas'
 import { useNewDeliveryContext } from '@/hooks/useNewDeliveryContext'
+import { useGenerateId } from '@/hooks/useGenerateId.ts'
+
+let id = 0
 
 export default function NewDeliveryStep3Form({
   control,
@@ -12,6 +15,7 @@ export default function NewDeliveryStep3Form({
 }: UseFormReturn<NewDeliveryStep3FormData>) {
   const { t: translate } = useTranslation()
   const { formsData } = useNewDeliveryContext()
+  const generateId = useGenerateId()
 
   const { remove, append } = useFieldArray({
     control,
@@ -26,7 +30,7 @@ export default function NewDeliveryStep3Form({
 
   // Initialize goodDetailsForms and selectedGoodTypes based on formsData.goods
   const goodDetailsFormsInitialValue = formsData.goods
-    ? formsData.goods.map((_: any, index: number) => index)
+    ? formsData.goods.map(() => generateId())
     : [0]
   const selectedGoodTypesInitialValue = formsData.goods
     ? formsData.goods.map((good: any) => good.goodTypeStep3 || '')
@@ -39,6 +43,7 @@ export default function NewDeliveryStep3Form({
 
   function onDeleteHandler(index: number) {
     // Не работи както трябва
+    console.log(index)
     remove(index) // This removes the item from goods array in formsData object
     setGoodDetailsForms((prev) => prev.filter((_, id) => id !== index))
     setSelectedGoodTypes((prev) => prev.filter((_, id) => id !== index))
@@ -46,7 +51,7 @@ export default function NewDeliveryStep3Form({
 
   function addGoodHandler() {
     append({ goodTypeStep3: '', goodQuantityStep3: 0 }) // add this object in goods array in formsData object
-    setGoodDetailsForms((prev) => [...prev, prev.length])
+    setGoodDetailsForms((prev) => [...prev, generateId()])
     setSelectedGoodTypes((prev) => [...prev, ''])
   }
 
@@ -70,16 +75,16 @@ export default function NewDeliveryStep3Form({
   useEffect(() => {
     if (formsData.goods) {
       // Update form values when formsData changes
-      setGoodDetailsForms(formsData.goods.map((_: any, index: number) => index))
+      setGoodDetailsForms(formsData.goods.map(() => generateId()))
       setSelectedGoodTypes(formsData.goods.map((good: any) => good.goodTypeStep3 || ''))
     }
   }, [formsData])
 
   return (
     <>
-      {goodDetailsForms.map((index) => (
+      {goodDetailsForms.map((id, index) => (
         <GoodDetailsForm
-          key={index}
+          key={id}
           control={control}
           errors={errors}
           goodType={availableGoodTypes(index)}
