337b7559e6b8e48bc4dbdccebdaddbfd3986cedd
Hristo Yonkov <hristopturs@gmail.com>
2024-06-26 13:16:48 +0300

    fix the error where border overflows the label

--- src/utils/forms/MoveEntryForm.tsx
+++ src/utils/forms/MoveEntryForm.tsx
@@ -33,22 +33,23 @@ export default function MoveEntryForm({ control, formState: { errors } }: UseFor
           />
         )}
       />
-        
+
       <Controller
         name="zone"
         control={control}
         render={({ field }) => (
           <FormControl fullWidth>
-            <InputLabel id="move-entry-checkbox-label">
+            <InputLabel id="move-entry-select-label">
               {translate('zones.moveEntry.zone')}
             </InputLabel>
             <Select
               {...field}
-              labelId="move-entry-checkbox-label"
-              id="move-entry-checkbox"
+              labelId="move-entry-select-label"
+              id="move-entry-select"
+              label={translate('zones.moveEntry.zone')}
               value={field.value || ''}
               onChange={(e) => field.onChange(e.target.value)}
-              input={<OutlinedInput />}>
+            >
               {zones.map((zone) => (
                 <MenuItem key={zone} value={zone}>
                   <ListItemText primary={zone} />
