b29bdbb9bb7fe193efd75981d9e218a0dd6853d8
Hristo Yonkov <hristopturs@gmail.com>
2024-06-27 12:45:48 +0300

    Update MoveEntryForm.tsx

--- src/utils/forms/MoveEntryForm.tsx
+++ src/utils/forms/MoveEntryForm.tsx
@@ -1,30 +1,36 @@
-import {
-  FormControl,
-  InputLabel,
-  ListItemText,
-  MenuItem,
-  Select,
-  TextField
-} from '@mui/material'
-import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward';
+import { Checkbox, FormControl, FormControlLabel, InputLabel, ListItemText, MenuItem, Select, TextField } from '@mui/material'
+import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward'
 import { Controller, UseFormReturn } from 'react-hook-form'
 import { useTranslation } from 'react-i18next'
 import { MoveEntryFormData } from '@/schemas/moveEntrySchema'
 
-export default function MoveEntryForm({ control, formState: { errors } }: UseFormReturn<MoveEntryFormData>) {
+interface MoveEntryFormProps {
+  methods: UseFormReturn<MoveEntryFormData>
+  quantity: number
+}
+
+export default function MoveEntryForm({ methods, quantity }: MoveEntryFormProps) {
+  const {
+    control,
+    formState: { errors }
+  } = methods
   const { t: translate } = useTranslation()
+
   const zones = ['Първа', 'Втора', 'Трета']
+
   return (
     <>
       <Controller
         name="quantity"
         control={control}
+        defaultValue={quantity}
         render={({ field }) => (
           <TextField
             {...field}
             label={translate('zones.moveEntry.quantity')}
             id="quantity"
             name="quantity"
+            value={quantity}
             required
             fullWidth
             autoFocus
@@ -33,7 +39,7 @@ export default function MoveEntryForm({ control, formState: { errors } }: UseFor
           />
         )}
       />
-      {/* TODO: Fix the arrow placement */}
+
       <ArrowDownwardIcon />
 
       <Controller
@@ -50,8 +56,7 @@ export default function MoveEntryForm({ control, formState: { errors } }: UseFor
               id="move-entry-select"
               label={translate('zones.moveEntry.zone')}
               value={field.value || ''}
-              onChange={(e) => field.onChange(e.target.value)}
-            >
+              onChange={(e) => field.onChange(e.target.value)}>
               {zones.map((zone) => (
                 <MenuItem key={zone} value={zone}>
                   <ListItemText primary={zone} />
@@ -59,6 +64,17 @@ export default function MoveEntryForm({ control, formState: { errors } }: UseFor
               ))}
             </Select>
           </FormControl>
+        )}
+      />
+
+      <Controller
+        name="isFinal"
+        control={control}
+        render={({ field }) => (
+          <FormControlLabel
+            control={<Checkbox {...field} checked={field.value} />}
+            label={translate('newZone.labels.isFinal')}
+          />
         )}
       />
     </>
