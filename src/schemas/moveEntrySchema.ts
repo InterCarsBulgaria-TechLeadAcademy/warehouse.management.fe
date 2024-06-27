59050385031d5d6a1d2d497700c135764eb36247
Hristo Yonkov <hristopturs@gmail.com>
2024-06-27 12:45:42 +0300

    Update moveEntrySchema.ts

--- src/schemas/moveEntrySchema.ts
+++ src/schemas/moveEntrySchema.ts
@@ -5,7 +5,8 @@ export const moveEntrySchema = yup.object({
     .number()
     .required('quantity.errors.name.required')
     .min(1, 'quantity.errors.name.max'),
-  zone: yup.string().required('Zone is required')
+  zone: yup.string().required('Zone is required'),
+  isFinal: yup.boolean().required('Confirm the form')
 })
 
 export interface MoveEntryFormData extends yup.InferType<typeof moveEntrySchema> {
