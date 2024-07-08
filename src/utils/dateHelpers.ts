7ef90e8312657186285d47371325f77eba72cc97
Hristo Boyadzhiev <hristo_boyadzhiev@abv.bg>
2024-07-08 07:47:00 +0300

    update code in dateHelper util

--- src/utils/dateHelpers.ts
+++ src/utils/dateHelpers.ts
@@ -1,9 +1,7 @@
 import dayjs from 'dayjs'
 
-export default function dateHelpers(date: string | Date, format?: string) {
-  function formatDate(date: string | Date, format: string = 'DD.MM.YYYY') {
-    return dayjs(date).format(format)
-  }
+type TDate = string | Date
 
-  return formatDate(date, format)
+export function formatDate(date: TDate, format = 'DD.MM.YYYY') {
+  return dayjs(date).format(format)
 }
