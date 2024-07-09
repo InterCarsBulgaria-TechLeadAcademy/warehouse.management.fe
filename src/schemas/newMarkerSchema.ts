d2d4d23fbcc26a59eb0d9e1190843d710f61a2bc
Hristo Boyadzhiev <hristo_boyadzhiev@abv.bg>
2024-07-09 17:45:44 +0300

    update new marker schema to be equal with BE validation

--- src/schemas/newMarkerSchema.ts
+++ src/schemas/newMarkerSchema.ts
@@ -3,8 +3,9 @@ import * as yup from 'yup'
 export const newMarkerSchema = yup.object({
   markerName: yup
     .string()
-    .required('newMarker.errors.name.required')
-    .max(25, 'newMarker.errors.name.max')
+    .required('newMarker.errors.required')
+    .min(2, 'newMarker.errors.min')
+    .max(100, 'newMarker.errors.max')
 })
 
 export interface NewMarkerFormData extends yup.InferType<typeof newMarkerSchema> {
