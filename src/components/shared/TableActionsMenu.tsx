83e7f6078c3113cde4c51452dcebb44123040956
Hristo Yonkov <hristopturs@gmail.com>
2024-07-09 13:15:22 +0300

    remove the warning dialog

--- src/components/shared/TableActionsMenu.tsx
+++ src/components/shared/TableActionsMenu.tsx
@@ -3,40 +3,27 @@ import Menu from '@mui/material/Menu'
 import MenuItem from '@mui/material/MenuItem'
 import MoreHorizIcon from '@mui/icons-material/MoreHoriz'
 import React from 'react'
-import WarningActionDialog from '../shared/WarningActionDialog'
 import { useTranslation } from 'react-i18next'
 
 interface TableActionsMenuProps {
   specificOptionHandler: Function;
   itemProps: string[];
-  page: string;
 }
 
-export default function TableActionsMenu({ specificOptionHandler, itemProps, page }: TableActionsMenuProps) {
+export default function TableActionsMenu({ specificOptionHandler, itemProps }: TableActionsMenuProps) {
   const { t: translate } = useTranslation()
   const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null)
   const open = Boolean(anchorEl)
-  const [selectedOption, setSelectedOption] = React.useState<string | null>(null)
 
   const handleClick = (event: React.MouseEvent<HTMLElement>) => {
     setAnchorEl(event.currentTarget)
   }
   const handleClose = () => {
-    setSelectedOption(null)
     setAnchorEl(null)
-  }
-
-  const onDiscardClick = () => {
-    handleClose()
-  }
-
-  const onConfirmClick = () => {
-    handleClose()
   }
 
   const actionHandler = (option: string) => {
     specificOptionHandler(option)
-    setSelectedOption(option)
     handleClose()
   }
 
@@ -68,19 +55,6 @@ export default function TableActionsMenu({ specificOptionHandler, itemProps, pag
           </MenuItem>
         ))}
       </Menu>
-
-      {selectedOption === 'Изтрий' && (
-        <WarningActionDialog
-          open={open}
-          title={translate(`${page}.deleteAction.title`)}
-          content={translate(`${page}.deleteAction.message`)}
-          discardText={translate(`${page}.deleteAction.labels.discard`)}
-          confirmText={translate(`${page}.deleteAction.labels.confirm`)}
-          onCloseDialog={handleClose}
-          onDiscardClick={onDiscardClick}
-          onConfirmClick={onConfirmClick}
-        />
-      )}
     </div>
   )
 }
