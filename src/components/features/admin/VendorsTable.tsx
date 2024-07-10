f7774f8e7aaa782f52a9267dcc4b67414cddaa2f
Hristo Yonkov <hristopturs@gmail.com>
2024-07-10 18:22:31 +0300

    integrate react querry to Vendors table (not finished)

--- src/components/features/admin/VendorsTable.tsx
+++ src/components/features/admin/VendorsTable.tsx
@@ -5,8 +5,12 @@ import { useTranslation } from 'react-i18next'
 import SearchInput from '../SearchInput'
 import { Autocomplete, TextField } from '@mui/material'
 import { Column } from '@/interfaces/column'
+import { useSuspenseQuery } from '@tanstack/react-query'
+import { getWarehouseManagementApi } from '@/services/generated-api'
+import { VendorDto } from '@/services/model'
 
 interface Row {
+  id: number
   name: string
   vendorNumber: number
   markers: string
@@ -16,9 +20,20 @@ interface Row {
 export default function VendorsTable() {
   const { t: translate } = useTranslation()
   const [searchTerm, setSearchTerm] = React.useState('')
+  const [page, setPage] = React.useState(0)
+  const [rowsPerPage, setRowsPerPage] = React.useState(10)
 
   const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setSearchTerm(event.target.value)
+  }
+
+  const onPageChange = (newPage: number) => {
+    setPage(newPage)
+  }
+
+  const onRowsPerPageChange = (newRowsPerPage: number) => {
+    setRowsPerPage(newRowsPerPage)
+    setPage(0)
   }
 
   const sortOptions = ['regular', 'admin']
@@ -31,20 +46,44 @@ export default function VendorsTable() {
     { key: 'actions', title: translate('vendors.table.actions'), minWidth: 50, align: 'right' }
   ]
 
-  const rowData: Row[] = [
-    {
-      name: 'Bosch',
-      vendorNumber: 1,
-      markers: 'Масло',
-      actions: <VendorTableActionsMenu />
-    },
-    {
-      name: 'Valeo',
-      vendorNumber: 2,
-      markers: 'Чистачки',
-      actions: <VendorTableActionsMenu />
+  // const rowData: Row[] = [
+  //   {
+  //     name: 'Bosch',
+  //     vendorNumber: 1,
+  //     markers: 'Масло',
+  //     actions: <VendorTableActionsMenu />
+  //   },
+  //   {
+  //     name: 'Valeo',
+  //     vendorNumber: 2,
+  //     markers: 'Чистачки',
+  //     actions: <VendorTableActionsMenu />
+  //   }
+  // ]
+
+  const { data } = useSuspenseQuery({
+    queryKey: ['markers'],
+    queryFn: () => {
+      return getWarehouseManagementApi().getApiVendorAll({
+        PageNumber: page + 1,
+        PageSize: rowsPerPage,
+        SearchQuery: searchTerm
+      })
     }
-  ]
+  })
+
+  function transformDataToRows(vendors: VendorDto[]): Row[] {
+    return vendors.map((vendor: VendorDto) => ({
+      id: vendor.id!,
+      name: vendor.name!,
+      vendorNumber: vendor.systemNumber,
+      markers: vendor.markers,
+      zones: vendor.zones,
+      actions: <VendorTableActionsMenu key={vendor.id} id={vendor.id!} name={vendor.name!} />
+    }))
+  }
+
+  const rowData = transformDataToRows(data || [])
 
   const filteredRows = rowData.filter((row: Row) => {
     return columnsData.some((column: Column<Row>) => {
@@ -53,7 +92,14 @@ export default function VendorsTable() {
   })
 
   return (
-    <DataTable columnsData={columnsData} rowData={filteredRows}>
+    <DataTable
+      columnsData={columnsData}
+      rowData={filteredRows}
+      page={page}
+      rowsPerPage={rowsPerPage}
+      onPageChange={onPageChange}
+      onRowsPerPageChange={onRowsPerPageChange}
+    >
       <SearchInput
         value={searchTerm}
         onChange={handleSearchChange}
