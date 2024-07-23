056086539e0234d2848e587bf30963c23695332b
Hristo Yonkov <hristopturs@gmail.com>
2024-07-23 10:56:06 +0300

    update code to be use only trough hoook useAuth

--- src/hooks/services/auth/useAuth.ts
+++ src/hooks/services/auth/useAuth.ts
@@ -1,10 +1,7 @@
-import * as React from 'react'
+import React, { useContext } from 'react';
 import { AuthContext } from '@/contexts/Auth'
 import Cookies from 'js-cookie';
 
-export const useAuth = () => {
-  return React.useContext(AuthContext)
-}
 
 const setTokenCookies = (accessToken: any, refreshToken: any) => {
   Cookies.set('accessToken', accessToken, { expires: 15 / 1440, secure: true, sameSite: 'Strict' }); // 15 minutes
@@ -19,71 +16,85 @@ const removeTokens = () => {
   Cookies.remove('refreshToken');
 };
 
-export async function loginUser() {
-  try {
-    const response = await fetch('https://dummyjson.com/auth/login', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({
-        username: 'emilys',
-        password: 'emilyspass',
-        expiresInMins: 1, // optional, defaults to 60
+export const useAuth = () => {
+  const { user, setUser } = useContext(AuthContext);
+
+  const loginUser = async () => {
+    try {
+      const response = await fetch('https://dummyjson.com/auth/login', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+          username: 'emilys',
+          password: 'emilyspass',
+          expiresInMins: 1, // optional, defaults to 60
+        })
       })
-    })
 
-    const data = await response.json()
-    setTokenCookies(data.token, data.refreshToken)
-    const requestedUser = await getUserFromCookies()
-    // requestedUser.role = 'regular' // Uncomment it to change role..
-    return { username: requestedUser.username, role: requestedUser.role };
-  } catch (error) {
-    console.error('Error during login:', error);
+      const data = await response.json()
+      setTokenCookies(data.token, data.refreshToken)
+      const requestedUser = await getUserFromCookies()
+      requestedUser.role = 'regular' // Uncomment it to change role..
+      setUser({ username: requestedUser.username, role: requestedUser.role });
+      return { username: requestedUser.username, role: requestedUser.role };
+    } catch (error) {
+      console.error('Error during login:', error);
+    }
   }
-}
 
-// Тази функция е допълнителна която служи за взимане на юзер данните и работи с логин-а заедно..
-export async function getUserFromCookies() {
-  const accessToken = getAccessToken()
-  const refreshToken = getRefreshToken()
+  // Тази функция е допълнителна която служи за взимане на юзер данните и работи с логин-а заедно..
+  const getUserFromCookies = async () => {
+    const accessToken = getAccessToken()
+    const refreshToken = getRefreshToken()
 
-  if (!accessToken) {
-    // Redirect to login..
-    // After that try to handle refresh token..
+    if (!accessToken) {
+      // Redirect to login..
+      // After that try to handle refresh token..
+      return null;
+    }
+
+    try {
+      const response = await fetch('https://dummyjson.com/auth/me', {
+        method: 'GET',
+        headers: {
+          'Authorization': `Bearer ${accessToken}`,
+        },
+      })
+
+      const user = await response.json()
+      return user;
+
+    } catch (error: any) {
+      if (error.response && error.response.status === 401) {
+        // Token might be expired, handle token refresh or re-login
+        removeTokens();
+        // Redirect to login or refresh tokens
+      } else {
+        console.error('Error fetching protected data', error);
+      }
+    }
     return null;
   }
 
-  try {
-    const response = await fetch('https://dummyjson.com/auth/me', {
-      method: 'GET',
-      headers: {
-        'Authorization': `Bearer ${accessToken}`,
-      },
-    })
+  const logoutUser = async () => {
+    try {
+      // logout from BE
 
-    const user = await response.json()
-    console.log('response', user);
-
-    return user;
-
-  } catch (error: any) {
-    if (error.response && error.response.status === 401) {
-      // Token might be expired, handle token refresh or re-login
       removeTokens();
-      // Redirect to login or refresh tokens
-    } else {
-      console.error('Error fetching protected data', error);
+      setUser(null);
+      return;
+    } catch (error) {
+      console.error('Error during logout:', error);
     }
   }
-  return null;
+
+  return {
+    user,
+    setUser,
+    loginUser,
+    getUserFromCookies,
+    logoutUser,
+  };
 }
 
-export async function logoutUser() {
-  try {
-    // logout from BE
-
-    removeTokens();
-    return;
-  } catch (error) {
-    console.error('Error during logout:', error);
-  }
-}
+export default useAuth;
