8060b57e3953377df0b4a098e2d19475d50bb008
Hristo Boyadzhiev <hristo_boyadzhiev@abv.bg>
2024-07-25 11:33:21 +0300

    update snackBar in usePostEntry

--- src/hooks/services/entries/usePostEntry.ts
+++ src/hooks/services/entries/usePostEntry.ts
@@ -3,7 +3,6 @@ import { getWarehouseManagementApi } from '@/services/generated-api'
 import { useMutation, useQueryClient } from '@tanstack/react-query'
 import { useTranslation } from 'react-i18next'
 
-// export default function usePostEntry(differenceTypeName: string) {
 export default function usePostEntry() {
   const { t: translate } = useTranslation()
   const { showSnackbar } = useSnackbar()
@@ -13,19 +12,12 @@ export default function usePostEntry() {
     mutationFn: getWarehouseManagementApi().postApiEntryAdd,
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ['entries'] })
-      //TODO: според мен не трябва да има snackBar
-      // showSnackbar({
-      //   message: translate('proba', {
-      //     //   name: differenceTypeName
-      //   }),
-      //   type: 'success'
-      // })
     },
     onError: () => {
-      // showSnackbar({
-      //   message: translate('proba'),
-      //   type: 'error'
-      // })
+      showSnackbar({
+        message: translate('zonesContent.newZonesContent.snackBar.error'),
+        type: 'error'
+      })
     }
   })
 
