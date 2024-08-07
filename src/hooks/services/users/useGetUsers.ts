2e8fe0be792c0a33dd20d45b4d5b96ddcd6a6c58
Hristo Yonkov <hristopturs@gmail.com>
2024-08-07 18:22:35 +0300

    fix it with prettier

--- src/hooks/services/users/useGetUsers.ts
+++ src/hooks/services/users/useGetUsers.ts
@@ -13,40 +13,40 @@
 // }
 
 export default function useGetUsers() {
-    const data: any[] = [
+  const data: any[] = [
+    {
+      id: 1, name: 'Ico', email: 'ico@abv.bg', role: 'regular', rights: [
         {
-            id: 1, name: 'Ico', email: 'ico@abv.bg', role: 'regular', rights: [
-                {
-                    rightId: 1,
-                    rightName: 'Създаване на зони'
-                },
-                {
-                    rightId: 2,
-                    rightName: 'Четене на зони'
-                },
-            ], dateCreated: '12-08-2024'
+          rightId: 1,
+          rightName: 'Създаване на зони'
         },
         {
-            id: 2, name: 'Kris', email: 'kris@abv.bg', role: 'admin', rights: [
-                {
-                    rightId: 1,
-                    rightName: 'Триене на зони'
-                },
-                {
-                    rightId: 2,
-                    rightName: 'Редактиране на зони'
-                },
-                {
-                    rightId: 3,
-                    rightName: 'Четене на зони'
-                },
-                {
-                    rightId: 4,
-                    rightName: 'Създаване на зони'
-                },
-            ], dateCreated: '12-08-2024'
+          rightId: 2,
+          rightName: 'Четене на зони'
         },
-    ];
+      ], dateCreated: '12-08-2024'
+    },
+    {
+      id: 2, name: 'Kris', email: 'kris@abv.bg', role: 'admin', rights: [
+        {
+          rightId: 1,
+          rightName: 'Триене на зони'
+        },
+        {
+          rightId: 2,
+          rightName: 'Редактиране на зони'
+        },
+        {
+          rightId: 3,
+          rightName: 'Четене на зони'
+        },
+        {
+          rightId: 4,
+          rightName: 'Създаване на зони'
+        },
+      ], dateCreated: '12-08-2024'
+    },
+  ];
 
-    return data;
+  return data;
 }
