212aec47648176c86dfd2795b73fbcaef4f129cc
Hristo Boyadzhiev <hristo_boyadzhiev@abv.bg>
2024-08-12 14:02:00 +0300

    add split functionality

--- src/components/features/forms/MoveEntryForm.tsx
+++ src/components/features/forms/MoveEntryForm.tsx
@@ -18,17 +18,27 @@ import { MoveEntryFormData, createMoveEntrySchema } from '@/schemas/moveEntrySch
 import { useMoveEntryDialog } from '@/hooks/dialogs/zonesContent/useMoveEntryDialog'
 import { useMoveEntry } from '@/hooks/services/entries/useMoveEntry.ts'
 import useGetZones from '@/hooks/services/zones/useGetZones.ts'
+import { useSplitEntry } from '@/hooks/services/entries/useSplitEntry'
 
 interface FormMoveEntryProps {
+  action: string
+  readOnlyQuantity?: boolean
   quantity: number
   entryId: number
   handleCloseForm: () => void
 }
 
-export default function MoveEntryForm({ quantity, handleCloseForm, entryId }: FormMoveEntryProps) {
+export default function MoveEntryForm({
+  action,
+  quantity,
+  handleCloseForm,
+  entryId,
+  readOnlyQuantity
+}: FormMoveEntryProps) {
   const [currentQuantity, setCurrentQuantity] = useState(quantity)
   const schema = createMoveEntrySchema(quantity)
   const entryMove = useMoveEntry()
+  const entrySplit = useSplitEntry()
   const zones = useGetZones()
 
   const {
@@ -45,7 +55,14 @@ export default function MoveEntryForm({ quantity, handleCloseForm, entryId }: Fo
   const { onCloseMoveEntryDialog } = useMoveEntryDialog()
 
   const handleFormSubmit: SubmitHandler<MoveEntryFormData> = (data) => {
-    entryMove.mutate({ id: entryId, newZoneId: Number(data.zone) })
+    switch (action) {
+      case 'move':
+        entryMove.mutate({ id: entryId, newZoneId: Number(data.zone) })
+        break
+      case 'split':
+        entrySplit.mutate({ count: data.quantity, newZoneId: Number(data.zone) })
+        break
+    }
     handleCloseForm()
     onCloseMoveEntryDialog()
   }
@@ -69,7 +86,7 @@ export default function MoveEntryForm({ quantity, handleCloseForm, entryId }: Fo
                 field.onChange(value)
               }}
               inputProps={{
-                readOnly: true
+                readOnly: readOnlyQuantity || false
               }}
               required
               fullWidth
@@ -120,3 +137,132 @@ export default function MoveEntryForm({ quantity, handleCloseForm, entryId }: Fo
     </Box>
   )
 }
+
+// import { useState } from 'react'
+// import {
+//   Box,
+//   FormControl,
+//   InputLabel,
+//   ListItemText,
+//   MenuItem,
+//   Select,
+//   TextField,
+//   FormHelperText,
+//   Button
+// } from '@mui/material'
+// import EastIcon from '@mui/icons-material/East'
+// import { Controller, useForm, SubmitHandler } from 'react-hook-form'
+// import { yupResolver } from '@hookform/resolvers/yup'
+// import { useTranslation } from 'react-i18next'
+// import { MoveEntryFormData, createMoveEntrySchema } from '@/schemas/moveEntrySchema'
+// import { useMoveEntryDialog } from '@/hooks/dialogs/zonesContent/useMoveEntryDialog'
+// import { useMoveEntry } from '@/hooks/services/entries/useMoveEntry.ts'
+// import useGetZones from '@/hooks/services/zones/useGetZones.ts'
+
+// interface FormMoveEntryProps {
+//   quantity: number
+//   entryId: number
+//   handleCloseForm: () => void
+//   readOnlyQuantity?: boolean
+// }
+
+// export default function MoveEntryForm({
+//   quantity,
+//   handleCloseForm,
+//   entryId,
+//   readOnlyQuantity
+// }: FormMoveEntryProps) {
+//   const [currentQuantity, setCurrentQuantity] = useState(quantity)
+//   const schema = createMoveEntrySchema(quantity)
+//   const entryMove = useMoveEntry()
+//   const zones = useGetZones()
+
+//   const {
+//     control,
+//     handleSubmit,
+//     formState: { errors }
+//   } = useForm<MoveEntryFormData>({
+//     resolver: yupResolver(schema),
+//     defaultValues: { quantity, zone: '' }
+//   })
+
+//   const { t: translate } = useTranslation()
+
+//   const { onCloseMoveEntryDialog } = useMoveEntryDialog()
+
+//   const handleFormSubmit: SubmitHandler<MoveEntryFormData> = (data) => {
+//     entryMove.mutate({ id: entryId, newZoneId: Number(data.zone) })
+//     handleCloseForm()
+//     onCloseMoveEntryDialog()
+//   }
+
+//   return (
+//     <Box component="form" onSubmit={handleSubmit(handleFormSubmit)}>
+//       <Box sx={{ display: 'flex', gap: '20px', alignItems: 'center' }}>
+//         <Controller
+//           name="quantity"
+//           control={control}
+//           render={({ field }) => (
+//             <TextField
+//               {...field}
+//               label={translate('zonesContent.table.actions.moveEntryForm.quantity')}
+//               id="quantity"
+//               type="number"
+//               value={currentQuantity}
+//               onChange={(e) => {
+//                 const value = Number(e.target.value)
+//                 setCurrentQuantity(value)
+//                 field.onChange(value)
+//               }}
+//               inputProps={{
+//                 readOnly: readOnlyQuantity || false
+//               }}
+//               required
+//               fullWidth
+//               autoFocus
+//               error={!!errors.quantity}
+//               helperText={
+//                 errors.quantity?.message ? translate(errors.quantity.message as string) : ''
+//               }
+//             />
+//           )}
+//         />
+
+//         <EastIcon />
+
+//         <Controller
+//           name="zone"
+//           control={control}
+//           render={({ field }) => (
+//             <FormControl fullWidth error={!!errors.zone}>
+//               <InputLabel id="move-entry-select-label">
+//                 {translate('zonesContent.table.actions.moveEntryForm.zone')}
+//               </InputLabel>
+//               <Select
+//                 {...field}
+//                 labelId="move-entry-select-label"
+//                 id="move-entry-select"
+//                 label={translate('zonesContent.table.actions.moveEntryForm.zone')}
+//                 value={field.value || ''}
+//                 onChange={(e) => field.onChange(e.target.value)}>
+//                 {zones.map((zone) => (
+//                   <MenuItem key={zone.id!} value={zone.id!}>
+//                     <ListItemText primary={zone.name!} />
+//                   </MenuItem>
+//                 ))}
+//               </Select>
+//               {errors.zone?.message && (
+//                 <FormHelperText>{translate(errors.zone.message as string)}</FormHelperText>
+//               )}
+//             </FormControl>
+//           )}
+//         />
+//       </Box>
+//       <Box sx={{ display: 'flex', justifyContent: 'right', gap: '1em' }}>
+//         <Button type="submit" variant="contained" sx={{ mt: 5 }}>
+//           {translate('zonesContent.table.actions.moveEntryForm.confirm')}
+//         </Button>
+//       </Box>
+//     </Box>
+//   )
+// }
---
+++ src/hooks/services/entries/useSplitEntry.ts
@@ -0,0 +1,49 @@
+import { useTranslation } from 'react-i18next'
+import { useSnackbar } from '@/hooks/useSnackbar.ts'
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+import { getWarehouseManagementApi } from '@/services/generated-api.ts'
+
+export function useSplitEntry() {
+  const { t: translate } = useTranslation()
+  const { showSnackbar } = useSnackbar()
+  const queryClient = useQueryClient()
+
+  const mutationUpdate = useMutation({
+    mutationFn: ({ count, newZoneId }: { count: number; newZoneId: number }) =>
+      getWarehouseManagementApi().postApiEntrySplit({ count, newZoneId }),
+
+    onSuccess: (_, id) => {
+      queryClient.invalidateQueries({ queryKey: ['entries'] })
+      showSnackbar({
+        message: translate('zonesContent.table.actions.splitEntry.snackBar.success', {
+          goodNumber: id
+        }),
+        type: 'success'
+      })
+    },
+    onError: (error: unknown) => {
+      // TODO: Handle errors better
+      // if (axios.isAxiosError(error)) {
+      //   if (error.response && error.response.status === 400) {
+      //     showSnackbar({
+      //       message: translate('vendors.table.actions.edit.errors.existVendor', {
+      //         vendorNumber: vendorNumber
+      //       }),
+      //       type: 'error'
+      //     })
+      //   } else {
+      //     showSnackbar({
+      //       message: translate('vendors.table.actions.edit.snackBar.error'),
+      //       type: 'error'
+      //     })
+      //   }
+      // } else {
+      showSnackbar({
+        message: translate('zonesContent.table.actions.splitEntry.snackBar.error'),
+        type: 'error'
+      })
+      // }
+    }
+  })
+  return mutationUpdate
+}
