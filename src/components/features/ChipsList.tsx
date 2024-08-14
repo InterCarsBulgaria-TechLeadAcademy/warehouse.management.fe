492aafe2295f032bcb402074194d9be0005fe981
Hristo Boyadzhiev <hristo_boyadzhiev@abv.bg>
2024-08-14 17:01:59 +0300

    update logic in chip

--- src/components/features/ChipsList.tsx
+++ src/components/features/ChipsList.tsx
@@ -5,6 +5,7 @@ import selectStatusColor from '@/utils/selectStatusColor'
 import { Box, Chip, ClickAwayListener, Grid, Typography } from '@mui/material'
 import Tooltip from '@mui/material/Tooltip'
 import * as React from 'react'
+import { Theme, useTheme } from '@mui/material/styles'
 
 interface MarkersProps {
   items: string[]
@@ -14,8 +15,9 @@ export default function ChipsList({ items }: MarkersProps) {
   const isSmallScreen = useIsSmallScreen()
   const { getChipLabel } = useChipLabel()
   const [open, setOpen] = React.useState(false)
+  const theme: Theme = useTheme()
 
-  const color = selectStatusColor(items[0] as StatusType)
+  let color = selectStatusColor(items[0] as StatusType)
   const chipLabel = `+ ${items.slice(2).length}`
 
   const handleTooltipClose = () => {
@@ -28,9 +30,20 @@ export default function ChipsList({ items }: MarkersProps) {
 
   return (
     <Box sx={{ display: 'flex', gap: '0.5em' }}>
-      {items.slice(0, 2).map((item, index) => (
-        <Chip key={index} label={getChipLabel(item)} color={color} />
-      ))}
+      {items.slice(0, 2).map((item, index) =>
+        color === 'secondary.light' ? (
+          <Chip
+            key={index}
+            label={getChipLabel(item)}
+            sx={{
+              backgroundColor: theme.palette.secondary.light,
+              color: theme.palette.mode === 'light' ? 'white' : ''
+            }}
+          />
+        ) : (
+          <Chip key={index} label={getChipLabel(item)} color={color} />
+        )
+      )}
 
       {items.length > 2 && !isSmallScreen && (
         <Tooltip
@@ -44,7 +57,17 @@ export default function ChipsList({ items }: MarkersProps) {
             </Box>
           }
           arrow>
-          <Chip label={chipLabel} color={color} />
+          {color === 'secondary.light' ? (
+            <Chip
+              label={chipLabel}
+              sx={{
+                backgroundColor: theme.palette.secondary.light,
+                color: theme.palette.mode === 'light' ? 'white' : ''
+              }}
+            />
+          ) : (
+            <Chip label={chipLabel} color={color} />
+          )}
         </Tooltip>
       )}
 
@@ -70,7 +93,18 @@ export default function ChipsList({ items }: MarkersProps) {
                     ))}
                   </Box>
                 }>
-                <Chip label={chipLabel} color={color} onClick={handleTooltipOpen} />
+                {color === 'secondary.light' ? (
+                  <Chip
+                    label={chipLabel}
+                    sx={{
+                      backgroundColor: theme.palette.secondary.light,
+                      color: theme.palette.mode === 'light' ? 'white' : ''
+                    }}
+                    onClick={handleTooltipOpen}
+                  />
+                ) : (
+                  <Chip label={chipLabel} color={color} onClick={handleTooltipOpen} />
+                )}
               </Tooltip>
             </div>
           </ClickAwayListener>
@@ -79,3 +113,85 @@ export default function ChipsList({ items }: MarkersProps) {
     </Box>
   )
 }
+
+// import useChipLabel from '@/hooks/useChipLabel'
+// import { useIsSmallScreen } from '@/hooks/useIsSmallScreen'
+// import { StatusType } from '@/types/StatusType'
+// import selectStatusColor from '@/utils/selectStatusColor'
+// import { Box, Chip, ClickAwayListener, Grid, Typography } from '@mui/material'
+// import Tooltip from '@mui/material/Tooltip'
+// import * as React from 'react'
+
+// interface MarkersProps {
+//   items: string[]
+// }
+
+// export default function ChipsList({ items }: MarkersProps) {
+//   const isSmallScreen = useIsSmallScreen()
+//   const { getChipLabel } = useChipLabel()
+//   const [open, setOpen] = React.useState(false)
+
+//   const color = selectStatusColor(items[0] as StatusType)
+//   const chipLabel = `+ ${items.slice(2).length}`
+
+//   const handleTooltipClose = () => {
+//     setOpen(false)
+//   }
+
+//   const handleTooltipOpen = () => {
+//     setOpen(true)
+//   }
+
+//   return (
+//     <Box sx={{ display: 'flex', gap: '0.5em' }}>
+//       {items.slice(0, 2).map((item, index) => (
+//         <Chip key={index} label={getChipLabel(item)} color={color} />
+//       ))}
+
+//       {items.length > 2 && !isSmallScreen && (
+//         <Tooltip
+//           title={
+//             <Box>
+//               {items.slice(2).map((item, index) => (
+//                 <Typography key={index} variant="body1">
+//                   {item}
+//                 </Typography>
+//               ))}
+//             </Box>
+//           }
+//           arrow>
+//           <Chip label={chipLabel} color={color} />
+//         </Tooltip>
+//       )}
+
+//       {items.length > 2 && isSmallScreen && (
+//         <Grid item>
+//           <ClickAwayListener onClickAway={handleTooltipClose}>
+//             <div>
+//               <Tooltip
+//                 PopperProps={{
+//                   disablePortal: true
+//                 }}
+//                 onClose={handleTooltipClose}
+//                 open={open}
+//                 disableFocusListener
+//                 disableHoverListener
+//                 disableTouchListener
+//                 title={
+//                   <Box>
+//                     {items.slice(2).map((item, index) => (
+//                       <Typography key={index} variant="body1">
+//                         {item}
+//                       </Typography>
+//                     ))}
+//                   </Box>
+//                 }>
+//                 <Chip label={chipLabel} color={color} onClick={handleTooltipOpen} />
+//               </Tooltip>
+//             </div>
+//           </ClickAwayListener>
+//         </Grid>
+//       )}
+//     </Box>
+//   )
+// }
--- src/utils/selectStatusColor.ts
+++ src/utils/selectStatusColor.ts
@@ -1,11 +1,16 @@
 import { StatusType } from '@/types/StatusType'
-import { Theme, useTheme } from '@mui/material/styles'
 
-type ColorType = 'default' | 'error' | 'success' | 'primary' | 'secondary' | 'info' | 'warning'
+type ColorType =
+  | 'default'
+  | 'error'
+  | 'success'
+  | 'primary'
+  | 'secondary'
+  | 'info'
+  | 'warning'
+  | 'secondary.light'
 
 export default function selectStatusColor(status: StatusType): ColorType {
-  const theme: Theme = useTheme()
-  console.log(theme)
   switch (status) {
     case 'Waiting':
       return 'secondary'
@@ -16,7 +21,7 @@ export default function selectStatusColor(status: StatusType): ColorType {
     case 'Approved':
       return 'success'
     case 'NoDifferences':
-      return 'success'
+      return 'secondary.light'
     default:
       return 'primary'
   }
