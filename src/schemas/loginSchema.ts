2365fcdbfc5cd45c1573da5d896d79f30acaa706
vexleet <abda70226@gmail.com>
2024-08-14 21:46:03 +0300

    Update src/schemas/loginSchema.ts

--- src/schemas/loginSchema.ts
+++ src/schemas/loginSchema.ts
@@ -4,10 +4,10 @@ export const loginSchema = yup
   .object({
     email: yup.string().required('login.errors.email.required').email('login.errors.email.invalid'),
     password: yup.string().required('login.errors.password.required')
-    // .min(4, 'login.errors.password.min')
-    // .max(10, 'login.errors.password.max')
-    // .matches(/[0-9]/, 'login.errors.password.digit')
-    // .matches(/[!@#$%^&*]/, 'login.errors.password.specialCharacter')
+    .min(4, 'login.errors.password.min')
+    .max(10, 'login.errors.password.max')
+    .matches(/[0-9]/, 'login.errors.password.digit')
+    .matches(/[!@#$%^&*]/, 'login.errors.password.specialCharacter')
   })
   .required()
 
