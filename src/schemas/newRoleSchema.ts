47c7cdc74c33a0a8c198ac29a97fab2603e49c99
Hristo Yonkov <hristopturs@gmail.com>
2024-08-15 10:46:21 +0300

    Create newRoleSchema.ts

---
+++ src/schemas/newRoleSchema.ts
@@ -0,0 +1,15 @@
+import * as yup from 'yup'
+
+export const newRoleSchema = yup.object({
+  name: yup
+    .string()
+    .required('Името е задължително')
+    .min(5, 'Името трябва да е минимум 5 символа')
+    .max(50, 'Името трябва да е максимум 50 символа'),
+  rights: yup.array().of(yup.string())
+})
+
+export interface NewRoleFormData extends yup.InferType<typeof newRoleSchema> {
+  name: string
+  rights: string[]
+}
