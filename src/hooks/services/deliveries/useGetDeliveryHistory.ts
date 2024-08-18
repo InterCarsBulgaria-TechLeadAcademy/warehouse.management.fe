c50b2cb33cf30588ef99bf74dd086a8b462b194c
vexleet <abda70226@gmail.com>
2024-08-18 19:21:45 +0300

    fix query key in get delivery history

--- src/hooks/services/deliveries/useGetDeliveryHistory.ts
+++ src/hooks/services/deliveries/useGetDeliveryHistory.ts
@@ -3,7 +3,7 @@ import { useSuspenseQuery } from '@tanstack/react-query'
 
 export default function useGetDeliveryHistory(id: number) {
   const { data } = useSuspenseQuery({
-    queryKey: ['deliveries', id], //add dependencies on deliveries
+    queryKey: ['deliveryHistory', id], //add dependencies on deliveries
     queryFn: () => {
       return getWarehouseManagementApi().getApiDeliveryHistoryId(id)
     }
