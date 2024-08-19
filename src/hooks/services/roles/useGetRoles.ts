7b1804c4ae3a5715b89b83c38ffacc9f6fbeb5e5
Hristo Yonkov <hristopturs@gmail.com>
2024-08-19 17:07:20 +0300

    add roles querry request

--- src/hooks/services/roles/useGetRoles.ts
+++ src/hooks/services/roles/useGetRoles.ts
@@ -1,52 +1,13 @@
-// import { getWarehouseManagementApi } from '@/services/generated-api'
-// import { useSuspenseQuery } from '@tanstack/react-query'
-
-// export default function useGetRoles() {
-//   const { data } = useSuspenseQuery({
-//     queryKey: ['roles'],
-//     queryFn: () => {
-//       return getWarehouseManagementApi().getApiRoleAll()
-//     }
-//   })
-
-//   return data
-// }
+import { getWarehouseManagementApi } from '@/services/generated-api'
+import { useSuspenseQuery } from '@tanstack/react-query'
 
 export default function useGetRoles() {
-    const data: any[] = [
-      {
-        id: 1, name: 'regular', permissions: [
-          {
-            rightId: 1,
-            permissionName: 'createZones'
-          },
-          {
-            rightId: 2,
-            permissionName: 'readZones'
-          },
-        ]
-      },
-      {
-        id: 2, name: 'admin', permissions: [
-          {
-            rightId: 1,
-            permissionName: 'deleteZones'
-          },
-          {
-            rightId: 2,
-            permissionName: 'editZones'
-          },
-          {
-            rightId: 3,
-            permissionName: 'readZones'
-          },
-          {
-            rightId: 4,
-            permissionName: 'createZones'
-          },
-        ]
-      },
-    ];
-  
-    return data;
-  }
+  const { data } = useSuspenseQuery({
+    queryKey: ['roles'],
+    queryFn: () => {
+      return getWarehouseManagementApi().getApiRoleAll()
+    }
+  })
+
+  return data
+}
