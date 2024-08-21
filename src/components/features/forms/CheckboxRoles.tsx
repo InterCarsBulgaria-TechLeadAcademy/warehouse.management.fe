f63f0dd12d5cdb3d7a04ac573a189bcd06f43593
Hristo Yonkov <hristopturs@gmail.com>
2024-08-21 16:42:51 +0300

    Update CheckboxRoles.tsx

--- src/components/features/forms/CheckboxRoles.tsx
+++ src/components/features/forms/CheckboxRoles.tsx
@@ -1,17 +1,21 @@
-import React, { useEffect, useState } from 'react';
-import { Box, FormControl, FormControlLabel, Checkbox, Typography } from '@mui/material';
-import { useTranslation } from 'react-i18next';
-import { RoutePermissionDto } from '@/services/model';
+import React, { useEffect, useState } from 'react'
+import { Box, FormControl, FormControlLabel, Checkbox, Typography } from '@mui/material'
+import { useTranslation } from 'react-i18next'
+import { RoutePermissionDto } from '@/services/model'
 
 interface CheckboxRolesProps {
-  permissions: Record<string, RoutePermissionDto[]>;
-  currentPermissions: object[];
+  permissions: Record<string, RoutePermissionDto[]>
+  currentPermissions: {
+    id: string
+    name: string
+  }[]
   onChange: (selectedIds: string[]) => void
 }
 
-export default function CheckboxRoles({ permissions, currentPermissions, onChange }: CheckboxRolesProps) {
-  const [permissionsState, setPermissionsState] = useState<Record<string, boolean>>({});
-
+export default function CheckboxRoles({ permissions, currentPermissions,onChange }: CheckboxRolesProps) {
+  const [permissionsState, setPermissionsState] = useState<Record<string, boolean>>({})
+  const [currentPermissionIds, seCurrentPermissionIds] = useState(currentPermissions.map((current) => current.id))
+  
   const permissionGroups = Object.keys(permissions)
   const firstColumns = permissionGroups.slice(0, 5)
   const secondColumns = permissionGroups.slice(5)
@@ -21,29 +25,31 @@ export default function CheckboxRoles({ permissions, currentPermissions, onChang
     const initialState: Record<string, boolean> = {};
     permissionGroups.forEach(group => {
       permissions[group].forEach(permission => {
-        initialState[permission.id!] = permissionIds.includes(permission.id!);
+        initialState[permission.id!] = currentPermissionIds.includes(permission.id!);
       });
     });
     setPermissionsState(initialState);
-  }, [permissions, permissionIds]);
+  }, [permissions, currentPermissions]);
 
   const handleCheckboxChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const { name, checked } = event.target;
+    const { name, checked } = event.target
     const updatedPermissionsState = {
       ...permissionsState,
-      [name]: checked,
-    };
-    setPermissionsState(updatedPermissionsState);
-    
-    const selectedIds = Object.keys(updatedPermissionsState).filter(key => updatedPermissionsState[key]);
-    onChange(selectedIds);
-  };
+      [name]: checked
+    }
+    setPermissionsState(updatedPermissionsState)
+
+    const selectedIds = Object.keys(updatedPermissionsState).filter(
+      (key) => updatedPermissionsState[key]
+    )
+    onChange(selectedIds)
+  }
 
   return (
     <>
       <Box display="flex" justifyContent="space-between" flexDirection={'column'}>
         <Box display={'flex'}>
-          {firstColumns.map((col) =>
+          {firstColumns.map((col) => (
             <Box>
               <FormControl component="fieldset">
                 <Typography variant="h6">{translate(col)}</Typography>
@@ -61,11 +67,11 @@ export default function CheckboxRoles({ permissions, currentPermissions, onChang
                 ))}
               </FormControl>
             </Box>
-          )}
+          ))}
         </Box>
 
         <Box display={'flex'}>
-          {secondColumns.map((col) =>
+          {secondColumns.map((col) => (
             <Box flex="1">
               <FormControl component="fieldset">
                 <Typography variant="h6">{translate(col)}</Typography>
@@ -83,9 +89,9 @@ export default function CheckboxRoles({ permissions, currentPermissions, onChang
                 ))}
               </FormControl>
             </Box>
-          )}
+          ))}
         </Box>
       </Box>
     </>
-  );
+  )
 }
