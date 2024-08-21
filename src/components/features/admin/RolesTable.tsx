813caa68ddce867f94cc9e33cb0ed32e501c8edc
Hristo Yonkov <hristopturs@gmail.com>
2024-08-21 17:10:47 +0300

    Update RolesTable.tsx

--- src/components/features/admin/RolesTable.tsx
+++ src/components/features/admin/RolesTable.tsx
@@ -51,7 +51,6 @@ export default function RolesTable() {
   ]
 
   function transformDataToRows(roles: RoleDto[]): Row[] {
-    console.log(roles);
     
     return roles.map((role: RoleDto) => ({
       id: role.id!,
