5b00452bcd91384ca8b431074ebbb97530a8682e
Hristo Yonkov <hristopturs@gmail.com>
2024-08-31 10:29:22 +0300

    update the code to wotk with new way for getting tokens

--- src/contexts/Auth.tsx
+++ src/contexts/Auth.tsx
@@ -1,5 +1,4 @@
 import axiosInstance from "@/hooks/services/auth/axiosInstance";
-import { getAccessToken, removeTokens } from "@/hooks/services/auth/useAuth";
 import React, { createContext, ReactNode, useEffect, useState } from "react";
 
 interface AuthContextProps {
@@ -26,16 +25,12 @@ export const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
 
   useEffect(() => {
     const fetchUser = async () => {
-      const accessToken = getAccessToken();
-      if (!accessToken) {
-        return;
-      }
       try {
-        const response = await axiosInstance.get('/auth/me');
-        setUserState({username: response.data.username, role: response.data.role});
+        const response = await axiosInstance.get('/api/User/me');
+        setUserState({username: response.data.username, role: response.data.roles[0]});
       } catch (error) {
         console.error('Error fetching user on initial load:', error);
-        removeTokens();
+        return
       }
     };
 
